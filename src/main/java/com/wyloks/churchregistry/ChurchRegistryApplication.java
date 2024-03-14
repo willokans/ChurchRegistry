fa2d9317d2166196fa5e21b561bf3e904e0f3aa2
willokans <Slide2014!>
2024-03-14 13:36:58 +0000

    Add more to initial commit

--- src/main/java/com/wyloks/churchregistry/ChurchRegistryApplication.java
+++ src/main/java/com/wyloks/churchregistry/ChurchRegistryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChurchRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChurchRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchRegistryApplication.class, args);
+    }
 
 }
