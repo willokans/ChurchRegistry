f45aecb4cca9e845c4279a0dd7e95660240b4621
willokans <Slide2014!>
2024-03-14 13:35:45 +0000

    project setup

---
+++ src/test/java/com/wyloks/churchregistry/ChurchRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyloks.churchregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
